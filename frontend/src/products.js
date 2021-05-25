3198bd81c4b4c9ce838b09dab8a822c7ade5cc68
TamarAsashvili <asashvili@gmail.com>
2021-05-25 23:27:37 +0200

    images

---
+++ frontend/src/products.js
@@ -0,0 +1,91 @@
+const products = [
+
+    {
+        _id: '1',
+        name: 'Airphone wireless blutooth headphones',
+        image: '/images/airprods.jpg',
+        description:
+            'lorem jxvdvdvv',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 88.5,
+        countInStock: 3,
+        rating: 4.5,
+        numReviews: 4,
+
+    },
+    {
+        _id: '2',
+        name: 'Airphone jjjjjjjjjjjjjjjjjjjwireless blutooth headphones',
+        image: '/images/3.jpg',
+        description:
+            'lorem jxvdvdvv',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 78.5,
+        countInStock: 8,
+        rating: 2.3,
+        numReviews: 7,
+
+    },
+    {
+        _id: '3',
+        name: 'Airphone jjjjjjjjjjjjjjjjjjjwireless blutooth headphones',
+        image: '/images/1.jpg',
+        description:
+            'lorem jxvdvdvv',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 8.8,
+        countInStock: 9,
+        rating: 1.6,
+        numReviews: 5,
+
+    },
+    {
+        _id: '4',
+        name: 'Airphone jjjjjjjjjjjjjjjjjjjwireless blutooth headphones',
+        image: '/images/4.jpg',
+        description:
+            'lorem jxvdvdvv',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 44.5,
+        countInStock: 4,
+        rating: 4.3,
+        numReviews: 4,
+
+    },
+    {
+        _id: '5',
+        name: 'Airphone jjjjjjjjjjjjjjjjjjjwireless blutooth headphones',
+        image: '/images/5.jpg',
+        description:
+            'lorem jxvdvdvv',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 55.5,
+        countInStock: 5,
+        rating: 2.3,
+        numReviews: 7,
+
+    },
+    {
+        _id: '6',
+        name: 'Airphone jjjjjjjjjjjjjjjjjjjwireless blutooth headphones',
+        image: '/images/6.jpg',
+        description:
+            'lorem jxvdvdvv',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 7.5,
+        countInStock: 7,
+        rating: 7.3,
+        numReviews: 7,
+
+    },
+
+
+]
+
+export default products;
