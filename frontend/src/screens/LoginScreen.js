9b4cc86b3398ee4731f82848744ed222ea159009
TamarAsashvili <asashvili@gmail.com>
2021-06-12 21:06:25 +0200

    User Login functionality

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -5,23 +5,35 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { Login } from '../actions/userActions'
+import { login } from '../actions/userActions'
 
-const LoginScreen = ({ location }) => {
+const LoginScreen = ({ location, history }) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    const dispatch = useDispatch()
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { loading, error, userInfo } = userLogin
 
     const redirect = location.search ? location.search.split('=')[1] : '/'
 
+    useEffect(() => {
+        if (userInfo) {
+            history.push(redirect)
+        }
+    }, [history, userInfo, redirect])
+
     const submitHandler = (e) => {
         e.preventDefault()
-        //Dispatch login
+        dispatch(login(email, password))
     }
 
     return (
         <FormContainer>
             <h1>Sing In</h1>
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader />}
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId='email'>
                     <Form.Label>
